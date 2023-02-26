3439bf892b9d9345e2b72d6e7da5cffae493992e
toannc <toannc@onemount.com>
2023-02-26 21:23:14 +0700

    change to rest api

--- api/index.go
+++ api/index.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	logger "github.com/sirupsen/logrus"
 	"github.com/toancrystal/coding-challenge-be/providers"
@@ -15,17 +16,30 @@ var connString = os.Getenv("POSTGRES_CON_STRING")
 var priRepo = repositories.NewPriceRepository(connString)
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	var response string
+	var response PriceResponse
 	priceRecord, err := priRepo.GetPrice("bitfinex", "ethusd")
 	if err != nil {
 		logger.Error("Get price from db error ", err)
 		price, err := client.GetPrice("bitfinex", "ethusd")
 		if err != nil {
 			logger.Error("Get price from api error ", err)
-			response = fmt.Sprintf("<h1>Error %s</h1>", err)
+			response = PriceResponse{
+				Meta: Meta{
+					Code:    http.StatusInternalServerError,
+					Message: err.Error(),
+				},
+			}
 		} else {
 			priRepo.CreatePrice("bitfinex", "ethusd", fmt.Sprintf("%f", price.Result.Price))
-			response = fmt.Sprintf("<h1>Eth price %f</h1>", price.Result.Price)
+			response = PriceResponse{
+				Meta: Meta{
+					Code:    http.StatusOK,
+					Message: "Success",
+				},
+				Data: Data{
+					Price: fmt.Sprintf("%f", price.Result.Price),
+				},
+			}
 		}
 	} else {
 		if priceRecord.UpdatedAt.Add(5 * time.Minute).Before(time.Now().UTC()) {
@@ -36,7 +50,16 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 				priRepo.UpdatePrice(priceRecord.Id, fmt.Sprintf("%f", price.Result.Price))
 			}
 		}
-		response = fmt.Sprintf("<h1>Eth price %f</h1>", priceRecord.Value)
+		response = PriceResponse{
+			Meta: Meta{
+				Code:    http.StatusOK,
+				Message: "Success",
+			},
+			Data: Data{
+				Price: priceRecord.Value,
+			},
+		}
 	}
-	fmt.Fprintf(w, response)
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
 }
