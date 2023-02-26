ef04dbe5ce4ba9d675ed65c87f0989ac46a880c8
toannc <toannc@onemount.com>
2023-02-26 21:55:03 +0700

    fix sql syntax

--- repositories/price_repository.go
+++ repositories/price_repository.go
@@ -26,7 +26,7 @@ type priceRepositoryImpl struct {
 
 func (p *priceRepositoryImpl) CreatePrice(exchange string, pair string, value string) (int64, error) {
 	current := time.Now()
-	result, err := p.dbCon.Exec("insert into prices(exchange, pair, value, created_at, updated_at) values ?, ?, ?, ?, ?",
+	result, err := p.dbCon.Exec("insert into prices(exchange, pair, value, created_at, updated_at) values $1, $2, $3, $4, $5",
 		exchange, pair, value, current, current)
 	if err != nil {
 		logger.Error("[PriceRepository][CreatePrice] Error while save price", err)
@@ -37,7 +37,7 @@ func (p *priceRepositoryImpl) CreatePrice(exchange string, pair string, value st
 }
 
 func (p *priceRepositoryImpl) UpdatePrice(id int, value string) (int64, error) {
-	rs, err := p.dbCon.Exec("update prices set updated_at = current_timestamp, value = ? where id = ?", value, id)
+	rs, err := p.dbCon.Exec("update prices set updated_at = current_timestamp, value = $1 where id = $2", value, id)
 	if err != nil {
 		logger.Error("[PriceRepository][CreatePrice] Error while UpdatePrice", err)
 		return 0, err
@@ -48,7 +48,8 @@ func (p *priceRepositoryImpl) UpdatePrice(id int, value string) (int64, error) {
 
 func (p *priceRepositoryImpl) GetPrice(exchange string, pair string) (*Price, error) {
 	var rs Price
-	err := p.dbCon.QueryRow("select id, pair, value, exchange, updated_at from prices where exchange=? and pair=?", exchange, pair).Scan(&rs)
+	row := p.dbCon.QueryRow("select id, pair, value, exchange, updated_at from prices where exchange = $1 and pair = $2", exchange, pair)
+	err := row.Scan(&rs.Id, &rs.Pair, &rs.Value, &rs.Exchange, &rs.UpdatedAt)
 	if err != nil {
 		logger.Errorf("[PriceRepository][GetPrice] Error while get price by exchange %s and pair %s, err=%+v",
 			exchange, pair, err)
--- repositories/price_repository_test.go
+++ repositories/price_repository_test.go
@@ -1,1 +1,37 @@
 package repositories
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestPriceRepositoryImpl_GetPrice(t *testing.T) {
+	var connString = os.Getenv("POSTGRES_CON_STRING")
+	repo := NewPriceRepository(connString)
+	price, err := repo.GetPrice("bitfinex", "ethusd")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(price)
+}
+
+func TestMockIPriceRepository_CreatePrice(t *testing.T) {
+	var connString = os.Getenv("POSTGRES_CON_STRING")
+	repo := NewPriceRepository(connString)
+	priceId, err := repo.CreatePrice("bitfinex", "ethusd", "122.2")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(priceId)
+}
+
+func TestPriceRepositoryImpl_UpdatePrice(t *testing.T) {
+	var connString = os.Getenv("POSTGRES_CON_STRING")
+	repo := NewPriceRepository(connString)
+	priceId, err := repo.UpdatePrice(1, "122.2")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(priceId)
+}
